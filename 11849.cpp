161a1a7011b539e094657f72ed1396b6d46235ac
evenpeng <shinycockorach@gmail.com>
2019-09-20 09:54:07 +0800

    :sparkles: Solve 11849

---
+++ 11849.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+    int n, m, cnt, x;
+    vector<int> ll(1000000);
+
+    while (1) {
+        scanf("%d %d\n", &n, &m);
+        if (n == 0 && m == 0) break;
+        cnt = 0;
+
+        for (int i = 0; i < n; ++i)
+            scanf("%d", &ll[i]);
+
+        int i = 0;
+        for (int j = 0; j < m; ++j) {
+            scanf("%d", &x);
+            while (i < n && ll[i] <= x) {
+                if (ll[i] == x) cnt++;
+                ++i;
+            }
+        }
+
+        printf("%d\n", cnt);
+    }
+
+    return 0;
+}
