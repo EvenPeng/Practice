103640c09dbf7a624fcf21d536fca8df787104bd
evenpeng <shinycockorach@gmail.com>
2019-09-22 12:24:33 +0800

    :sparkles: Optimize by skipping negative

--- 507.cpp
+++ 507.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
 int main(void)
 {
-    int T, n, b, e, m, p, tm;
-    int len, nice[20000], idx[20000];
+    int  T, n, b, e, m, p, tm, s;
+    int  len, nice[20000], oidx[20000], ss[20000];
+    int *idx = oidx + 1;
+    oidx[0]  = 0;
 
     scanf("%d", &T);
 
@@ -28,26 +30,34 @@ int main(void)
                 len++;
             }
         }
+        if (nice[len - 1] < 0) len--;
+
+        for (int i = 2 + nice[0] > 0 ? 0 : 1; i < len; i += 2)
+            ss[i] = nice[i] + nice[i - 1];
 
         m = 0;
-        p = 0;
-        for (int i = 0; i < len; ++i) {
-            if (nice[i] > 0) {
-                tm = 0;
-                for (int j = i; j < len; ++j) {
-                    tm += nice[j];
-                    if (tm > m || (tm == m && p < (idx[j] - (i == 0 ? 0 : idx[i - 1])) + 1)) {
-                        b = i == 0 ? 0 : idx[i - 1];
-                        e = idx[j];
-                        p = e - b + 1;
-                        m = tm;
-                    }
+        p = -1;
+        for (int i = nice[0] > 0 ? 0 : 1; i < len; i += 2) {
+            tm = nice[i];
+            if (tm > m || (tm == m && p < idx[i] - idx[i - 1])) {
+                b = i;
+                e = i;
+                p = idx[i] - idx[i - 1];
+                m = tm;
+            }
+            for (int j = i + 2; j < len; j += 2) {
+                tm += ss[j];
+                if (tm > m || (tm == m && p < idx[j] - idx[i - 1])) {
+                    b = i;
+                    e = j;
+                    p = idx[e] - idx[b - 1];
+                    m = tm;
                 }
             }
         }
 
         if (m > 0)
-            printf("The nicest part of route %d is between stops %d and %d\n", t, b + 1, e + 1);
+            printf("The nicest part of route %d is between stops %d and %d\n", t, idx[b - 1] + 1, idx[e] + 1);
         else
             printf("Route %d has no nice parts\n", t);
     }
