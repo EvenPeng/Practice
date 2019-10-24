a195d5ac3ef3aba62329f2d355a69ea9bd2a2a43
evenpeng <shinycockorach@gmail.com>
2019-10-24 22:06:08 +0800

    :zap: Search from the longest segment

--- 11235.cpp
+++ 11235.cpp
@@ -2,18 +2,18 @@
 #include <iostream>
 
 struct Number {
-    int value, index;
+    int value, beg, end, size;
 };
 
 bool compare_number(const Number &x, const Number &y)
 {
-    return x.index < y.index;
+    return x.size == y.size ? x.beg < y.beg : x.size > y.size;
 }
 
 int main(void)
 {
     int    length, num_query, num_value, tmp;
-    int    beg, end, idx, freq_idx, freq_size;
+    int    beg, end, idx, freq_size;
     Number numbers[100001], beg_num;
 
     while (1) {
@@ -23,41 +23,54 @@ int main(void)
 
         num_value = 0;
         std::cin >> numbers[0].value;
-        numbers[0].index = 1;
+        numbers[0].beg  = 1;
+        numbers[0].size = 1;
         for (int i = 1; i < length; ++i) {
             std::cin >> tmp;
             if (tmp == numbers[num_value].value)
-                ++numbers[num_value].index;
+                ++numbers[num_value].size;
             else {
-                numbers[++num_value].value = tmp;
-                numbers[num_value].index   = i + 1;
+                if (numbers[num_value].size > 1) {
+                    numbers[num_value].end =
+                        numbers[num_value].beg + numbers[num_value].size - 1;
+                    numbers[++num_value].value = tmp;
+                }
+                else {
+                    numbers[num_value].value = tmp;
+                }
+                numbers[num_value].beg  = i + 1;
+                numbers[num_value].size = 1;
             }
         }
+        numbers[num_value].end =
+            numbers[num_value].beg + numbers[num_value].size - 1;
         ++num_value;
+        std::sort(numbers, numbers + num_value, compare_number);
 
-        for (int i = 0; i < num_query; ++i) {
+        for (int k = 0; k < num_query; ++k) {
             std::cin >> beg >> end;
-            beg_num.index = beg;
-            idx = int(std::lower_bound(numbers, numbers + num_value, beg_num,
-                                       compare_number) -
-                      numbers);
-
-            if (numbers[idx].index >= end) {
-                freq_size = end - beg + 1;
-            }
-            else {
-                freq_idx  = idx;
-                freq_size = numbers[idx].index - beg + 1;
-                while (++idx < num_value && numbers[idx].index < end) {
-                    if (numbers[idx].index - numbers[idx - 1].index >
-                        freq_size) {
-                        freq_size = numbers[idx].index - numbers[idx - 1].index;
-                        freq_idx  = idx;
+            freq_size = 1;
+            for (int i = 0; i < num_value && numbers[i].size > freq_size; ++i) {
+                if (numbers[i].end < beg || end < numbers[i].beg) continue;
+                if (numbers[i].beg < beg) {
+                    if (numbers[i].end < end) {
+                        freq_size =
+                            std::max(freq_size, numbers[i].end - beg + 1);
+                    }
+                    else {
+                        freq_size = end - beg + 1;
+                        break;
                     }
                 }
-                if (end - numbers[idx - 1].index > freq_size) {
-                    freq_size = end - numbers[idx - 1].index;
-                    freq_idx  = idx;
+                else {
+                    if (numbers[i].end <= end) {
+                        freq_size = numbers[i].size;
+                        break;
+                    }
+                    else {
+                        freq_size =
+                            std::max(freq_size, end - numbers[i].beg + 1);
+                    }
                 }
             }
             std::cout << freq_size << std::endl;
